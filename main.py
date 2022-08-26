8cdd0ababf5fef996c1032dcbd66d2265faf606a
CUN-bjy <wnsdlqjtm@naver.com>
2022-08-26 11:03:39 +0900

    initial codebase

---
+++ main.py
@@ -0,0 +1,209 @@
+##########################
+# Autor: Junyeob Baek
+# email: wnsdlqjtm@gmail.com
+##########################
+
+import secrets
+
+import easydict
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+from tqdm import tqdm
+
+from models import LSTMAE, LSTMVAE
+from utils.MovingMNIST import MovingMNIST
+
+writer = SummaryWriter()
+
+## visualization
+def imshow(past_data, title="MovingMNIST"):
+    num_img = len(past_data)
+    fig = fig = plt.figure(figsize=(4 * num_img, 4))
+
+    for idx in range(1, num_img + 1):
+        ax = fig.add_subplot(1, num_img + 1, idx)
+        ax.imshow(past_data[idx - 1])
+    plt.suptitle(title, fontsize=30)
+    plt.savefig(f"{title}")
+    plt.close()
+
+
+def train(args, model, train_loader, test_loader):
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+
+    ## interation setup
+    epochs = tqdm(range(args.max_iter // len(train_loader) + 1))
+
+    ## training
+    count = 0
+    for epoch in epochs:
+        model.train()
+        optimizer.zero_grad()
+        train_iterator = tqdm(
+            enumerate(train_loader), total=len(train_loader), desc="training"
+        )
+
+        for i, batch_data in train_iterator:
+
+            if count > args.max_iter:
+                return model
+            count += 1
+
+            future_data, past_data = batch_data
+
+            ## reshape
+            batch_size = past_data.size(0)
+            example_size = past_data.size(1)
+            image_size = past_data.size(2), past_data.size(3)
+            past_data = (
+                past_data.view(batch_size, example_size, -1).float().to(args.device)
+            )
+            # future_data = future_data.view(batch_size, example_size, -1).float().to(args.device)
+
+            mloss, recon_x, info = model(past_data)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            mloss.mean().backward()
+            optimizer.step()
+
+            train_iterator.set_postfix({"train_loss": float(mloss.mean())})
+        writer.add_scalar("train_loss", float(mloss.mean()), epoch)
+
+        model.eval()
+        eval_loss = 0
+        test_iterator = tqdm(
+            enumerate(test_loader), total=len(test_loader), desc="testing"
+        )
+
+        with torch.no_grad():
+            for i, batch_data in test_iterator:
+                future_data, past_data = batch_data
+
+                ## reshape
+                batch_size = past_data.size(0)
+                example_size = past_data.size(1)
+                past_data = (
+                    past_data.view(batch_size, example_size, -1).float().to(args.device)
+                )
+                # future_data = future_data.view(batch_size, example_size, -1).float().to(args.device)
+
+                mloss, recon_x, info = model(past_data)
+
+                eval_loss += mloss.mean().item()
+
+                test_iterator.set_postfix({"eval_loss": float(mloss.mean())})
+
+                if i == 0:
+                    nhw_orig = past_data[0].view(example_size, image_size[0], -1)
+                    nhw_recon = recon_x[0].view(example_size, image_size[0], -1)
+                    imshow(nhw_orig.cpu(), f"orig{epoch}")
+                    imshow(nhw_recon.cpu(), f"recon{epoch}")
+                    # writer.add_images(f"original{i}", nchw_orig, epoch)
+                    # writer.add_images(f"reconstructed{i}", nchw_recon, epoch)
+
+        eval_loss = eval_loss / len(test_loader)
+        writer.add_scalar("eval_loss", float(eval_loss), epoch)
+        print("Evaluation Score : [{}]".format(eval_loss))
+
+    return model
+
+
+if __name__ == "__main__":
+
+    # training dataset
+    train_set = MovingMNIST(
+        root=".data/mnist",
+        train=True,
+        download=True,
+        transform=transforms.ToTensor(),
+        target_transform=transforms.ToTensor(),
+    )
+
+    # test dataset
+    test_set = MovingMNIST(
+        root=".data/mnist",
+        train=False,
+        download=True,
+        transform=transforms.ToTensor(),
+        target_transform=transforms.ToTensor(),
+    )
+
+    args = easydict.EasyDict(
+        {
+            "batch_size": 512,
+            "device": torch.device("cuda")
+            if torch.cuda.is_available()
+            else torch.device("cpu"),
+            "input_size": 4096,
+            "hidden_size": 2048,
+            "latent_size": 1024,
+            "learning_rate": 0.001,
+            "max_iter": 1000,
+        }
+    )
+
+    batch_size = args.batch_size
+    input_size = args.input_size
+    hidden_size = args.hidden_size
+    latent_size = args.latent_size
+
+    # define LSTM-based VAE model
+    model = LSTMVAE(input_size, hidden_size, latent_size, device=args.device)
+    model.to(args.device)
+
+    # reduce dataset size(customization for quick experiments)
+    # tr_split_len, te_split_len = 9000, 1000
+    # part_tr = torch.utils.data.random_split(
+    #     train_set, [tr_split_len, len(train_set) - tr_split_len]
+    # )[0]
+    # part_te = torch.utils.data.random_split(
+    #     test_set, [te_split_len, len(test_set) - te_split_len]
+    # )[0]
+
+    # convert to format of data loader
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_set, batch_size=args.batch_size, shuffle=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_set, batch_size=args.batch_size, shuffle=False
+    )
+
+    # training
+    train(args, model, train_loader, test_loader)
+
+    # save model
+    id_ = secrets.token_hex(nbytes=4)
+    torch.save(model.state_dict(), f"lstmvae{id_}.model")
+
+    # load model
+    model_to_load = LSTMVAE(input_size, hidden_size, latent_size, device=args.device)
+    model_to_load.to(args.device)
+    model_to_load.load_state_dict(torch.load(f"lstmvae{id_}.model"))
+    model_to_load.eval()
+
+    # show results
+    ## past_data, future_data -> shape: (10,10)
+    future_data, past_data = train_set[0]
+
+    ## reshape
+    example_size = past_data.size(0)
+    image_size = past_data.size(1), past_data.size(2)
+    past_data = past_data.view(example_size, -1).float().to(args.device)
+    _, recon_data, info = model_to_load(past_data.unsqueeze(0))
+
+    nhw_orig = past_data.view(example_size, image_size[0], -1).cpu()
+    nhw_recon = (
+        recon_data.squeeze(0)
+        .view(example_size, image_size[0], -1)
+        .detach()
+        .cpu()
+        .numpy()
+    )
+
+    imshow(nhw_orig, title=f"final_input{id_}")
+    imshow(nhw_recon, title=f"final_output{id_}")
+    plt.show()
---
+++ models.py
@@ -0,0 +1,197 @@
+##########################
+# Autor: Junyeob Baek
+# email: wnsdlqjtm@gmail.com
+##########################
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_size=4096, hidden_size=1024, num_layers=2):
+        super(Encoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(
+            input_size,
+            hidden_size,
+            num_layers,
+            batch_first=True,
+            bidirectional=False,
+        )
+
+    def forward(self, x):
+        # x: tensor of shape (batch_size, seq_length, hidden_size)
+        outputs, (hidden, cell) = self.lstm(x)
+        return (hidden, cell)
+
+
+class Decoder(nn.Module):
+    def __init__(
+        self, input_size=4096, hidden_size=1024, output_size=4096, num_layers=2
+    ):
+        super(Decoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(
+            input_size,
+            hidden_size,
+            num_layers,
+            batch_first=True,
+            bidirectional=False,
+        )
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, hidden):
+        # x: tensor of shape (batch_size, seq_length, hidden_size)
+        output, (hidden, cell) = self.lstm(x, hidden)
+        prediction = self.fc(output)
+        return prediction, (hidden, cell)
+
+
+class LSTMVAE(nn.Module):
+    """LSTM-based Variational Auto Encoder"""
+
+    def __init__(
+        self, input_size, hidden_size, latent_size, device=torch.device("cuda")
+    ):
+        """
+        input_size: int, batch_size x sequence_length x input_dim
+        hidden_size: int, output size of LSTM AE
+        latent_size: int, latent z-layer size
+        num_lstm_layer: int, number of layers in LSTM
+        """
+        super(LSTMVAE, self).__init__()
+        self.device = device
+
+        # dimensions
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.latent_size = latent_size
+        self.num_layers = 1
+
+        # lstm ae
+        self.lstm_enc = Encoder(
+            input_size=input_size, hidden_size=hidden_size, num_layers=self.num_layers
+        )
+        self.lstm_dec = Decoder(
+            input_size=latent_size,
+            output_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=self.num_layers,
+        )
+
+        self.fc21 = nn.Linear(self.hidden_size, self.latent_size)
+        self.fc22 = nn.Linear(self.hidden_size, self.latent_size)
+        self.fc3 = nn.Linear(self.latent_size, self.hidden_size)
+
+    def reparametize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        noise = torch.randn_like(std).to(self.device)
+
+        z = mu + noise * std
+        return z
+
+    def forward(self, x):
+        batch_size, seq_len, feature_dim = x.shape
+
+        # encode input space to hidden space
+        enc_hidden = self.lstm_enc(x)
+        enc_h = enc_hidden[0].view(batch_size, self.hidden_size).to(self.device)
+
+        # extract latent variable z(hidden space to latent space)
+        mean = self.fc21(enc_h)
+        logvar = self.fc22(enc_h)
+        z = self.reparametize(mean, logvar)  # batch_size x latent_size
+
+        # decode latent space to input space
+        z = z.repeat(1, seq_len, 1)
+        z = z.view(batch_size, seq_len, self.latent_size).to(self.device)
+        reconstruct_output, hidden = self.lstm_dec(z, enc_hidden)
+
+        x_hat = reconstruct_output
+
+        # calculate vae loss
+        losses = self.loss_function(x_hat, x, mean, logvar)
+        m_loss, recon_loss, kld_loss = (
+            losses["loss"],
+            losses["Reconstruction_Loss"],
+            losses["KLD"],
+        )
+
+        return m_loss, x_hat, (recon_loss, kld_loss)
+
+    def loss_function(self, *args, **kwargs) -> dict:
+        """
+        Computes the VAE loss function.
+        KL(N(\mu, \sigma), N(0, 1)) = \log \frac{1}{\sigma} + \frac{\sigma^2 + \mu^2}{2} - \frac{1}{2}
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        recons = args[0]
+        input = args[1]
+        mu = args[2]
+        log_var = args[3]
+
+        kld_weight = 0.00025  # Account for the minibatch samples from the dataset
+        recons_loss = F.mse_loss(recons, input)
+
+        kld_loss = torch.mean(
+            -0.5 * torch.sum(1 + log_var - mu**2 - log_var.exp(), dim=1), dim=0
+        )
+
+        loss = recons_loss + kld_weight * kld_loss
+        return {
+            "loss": loss,
+            "Reconstruction_Loss": recons_loss.detach(),
+            "KLD": -kld_loss.detach(),
+        }
+
+
+class LSTMAE(nn.Module):
+    """LSTM-based Auto Encoder"""
+
+    def __init__(self, input_size, hidden_size, latent_size, device=torch.device("cuda")):
+        """
+        input_size: int, batch_size x sequence_length x input_dim
+        hidden_size: int, output size of LSTM AE
+        latent_size: int, latent z-layer size
+        num_lstm_layer: int, number of layers in LSTM
+        """
+        super(LSTMAE, self).__init__()
+        self.device = device
+
+        # dimensions
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.latent_size = latent_size
+
+        # lstm ae
+        self.lstm_enc = Encoder(
+            input_size=input_size,
+            hidden_size=hidden_size,
+        )
+        self.lstm_dec = Decoder(
+            input_size=input_size,
+            output_size=input_size,
+            hidden_size=hidden_size,
+        )
+
+        self.criterion = nn.MSELoss()
+
+    def forward(self, x):
+        batch_size, seq_len, feature_dim = x.shape
+
+        enc_hidden = self.lstm_enc(x)
+
+        temp_input = torch.zeros((batch_size, seq_len, feature_dim), dtype=torch.float).to(
+            self.device
+        )
+        hidden = enc_hidden
+        reconstruct_output, hidden = self.lstm_dec(temp_input, hidden)
+        reconstruct_loss = self.criterion(reconstruct_output, x)
+
+        return reconstruct_loss, reconstruct_output, (0, 0)
